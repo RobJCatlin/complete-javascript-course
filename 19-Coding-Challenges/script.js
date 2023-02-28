0d7349ecbc65e7ba4facf48753203710f97d898e
RobJCatlin <78025477+RobJCatlin@users.noreply.github.com>
2023-02-28 23:39:25 +0000

    Coding Challenge #4

--- 19-Coding-Challenges/script.js
+++ 19-Coding-Challenges/script.js
@@ -1,5 +1,3 @@
-// JavaScript Fundamentals – Part 1
-
 // Coding Challenge #1
 
 // Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula:
@@ -27,9 +25,16 @@ function CompareBMI(user1, user2) {
   //how can I DRY loop this shit?
   var user1BMI = user1.mass / (user1.height * user1.height);
   var user2BMI = user2.mass / (user2.height * user2.height);
+
+  if (user1BMI > user2BMI) {
+    console.log(`${user1.name} has a higher BMI of ${user1BMI}`);
+  } else {
+    console.log(`${user2.name} has a higher BMI of ${user2BMI}`);
+  }
   let markHigherBMI;
   Mark.BMI = user1BMI;
-  return user1BMI > user2BMI ? (markHigherBMI = true) : null;
+  user1BMI > user2BMI ? (markHigherBMI = true) : null;
+  return;
 }
 // 3. Create a Boolean variable 'markHigherBMI' containing information about
 // whether Mark has a higher BMI than John.
@@ -37,3 +42,94 @@ function CompareBMI(user1, user2) {
 // Test data:
 // § Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
 // § Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Coding Challenge #2
+
+// Use the BMI example from Challenge #1, and the code you already wrote, and improve it.
+
+// Your tasks:
+// 1. Print a nice output to the console, saying who has the higher BMI. The message is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+
+// 2. Use a template literal to include the BMI values in the outputs. Example:"Mark's BMI (28.3) is higher than John's (23.9)!"
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Coding Challenge #3
+
+// There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins a trophy!
+// Your tasks:
+let koalasScores = [88, 91, 110];
+let dolphinsScores = [96, 108, 89];
+
+const averageScoreCalculator = (arr) =>
+  arr.reduce((preVal, currVal) => preVal + currVal, 0) / arr.length;
+
+let dolphinsAverageScore = averageScoreCalculator(dolphinsScores);
+let koalasAverageScore = averageScoreCalculator(koalasScores);
+
+console.log(`The Dolphins average score is ${dolphinsAverageScore}`);
+console.log(`The Koalas average score is ${koalasAverageScore}`);
+
+// 1. Calculate the average score for each team,using the test data below
+
+// 2. Compare the team's average scores to determine the winner of the competition,
+// and print it to the console. Don't forget that there can be a draw, so test for that
+// as well (draw means they have the same average score)
+if (
+  dolphinsAverageScore === koalasAverageScore &&
+  koalasAverageScore >= 100 &&
+  dolphinsAverageScore >= 100
+) {
+  console.log("It's a draw");
+} else if (
+  dolphinsAverageScore > koalasAverageScore &&
+  dolphinsAverageScore >= 100
+) {
+  console.log(
+    `The Dolphins win with an average score of ${dolphinsAverageScore}`
+  );
+} else if (
+  koalasAverageScore > dolphinsAverageScore &&
+  koalasAverageScore >= 100
+) {
+  console.log(`The Koalas win with an average score of ${koalasAverageScore}`);
+}
+// 3. Bonus1: Include a requirement for a minimum score of 100. With this rule, a
+// team only wins if it has a higher score than the other team, and the same time a score of at least 100 points. Hint: Use a logical operator to test for minimum score, as well as multiple else-if blocks 😉
+
+// 4. Bonus2: Minimum score also applies to a draw! So a draw only happens when both teams have the same score and both have a score greater or equal 100 points. Otherwise, no team wins the trophy
+
+// Test data:
+// § Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+// § Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+// § Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Coding Challenge #4
+
+// Steven wants to build a very simple tip calculator for whenever he goes eating in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+
+// Your tasks:
+// 1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can start with an if/else statement, and then try to convert it to a ternary operator!)
+let bill = 275;
+
+let tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+console.log(`Bill: $${bill}, Tip: $${tip}, Total: $${tip + bill}`);
+
+// 2. Print a string to the console containing the bill value,the tip,and the final value (bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value 316.25”
+
+// Test data:
+// § Data 1: Test for bill values 275, 40 and 430
+
+//Hints:
+// § To calculate 20% of a value, simply multiply it by 20/100 = 0.2 § Value X is between 50 and 300, if it's>= 50 && <= 300😉
