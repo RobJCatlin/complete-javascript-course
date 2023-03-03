1ed0164e303769d653470f75cb3e4c4da486e369
RobJCatlin <78025477+RobJCatlin@users.noreply.github.com>
2023-03-03 01:07:46 +0000

    JavaScript Fundamentals – Part 2 - Coding Challenge #2

--- 19-Coding-Challenges/script.js
+++ 19-Coding-Challenges/script.js
@@ -158,17 +158,19 @@ const calcAverage = (arr) =>
 
 // 2. Use the function to calculate the average for both teams
 
-console.log(calcAverage(dolphinsScore));
-console.log(calcAverage(koalasScore));
+const dolphinAvg = calcAverage(dolphinsScore);
+const koalaAvg = calcAverage(koalasScore);
 // 3. Create a function 'checkWinner' that takes the average score of each team
 // as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)"
 
 // 4. Use the 'checkWinner' function to determine the winner for both Data1 and Data 2
-const checkWinner = (data1, data2) => {
-  data1;
-};
+const checkWinner = (data1, data2) =>
+  data1 > data2 && data1 >= data2 * 2
+    ? `Dolphins win (${data1} vs. ${data2})`
+    : `Koalas win (${data2} vs. ${data1})`;
 // 5. Ignore draws this time
-
+const data = checkWinner(dolphinAvg, koalaAvg);
+console.log(data);
 // Test data:
 // § Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
 // § Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
@@ -177,3 +179,27 @@ const checkWinner = (data1, data2) => {
 // § To calculate average of 3 values, add them all together and divide by 3
 // § To check if number A is at least double number B, check for A >= 2 * B.
 // Apply this to the team's average scores
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Coding Challenge #2
+
+// Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+// Your tasks:
+
+// 1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100
+const calcTip = (billValue) =>
+  billValue >= 50 && billValue <= 300 ? billValue * 0.15 : billValue * 0.2;
+// 2. And now let's use arrays! So create an array 'bills' containing the test data below
+
+const bills = [125, 555, 44];
+// 3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before
+const tips = [calcTip(125), calcTip(555), calcTip(44)];
+// 4. Bonus: Create an array 'total' containing the total values, so the bill + tip Test data: 125, 555 and 44
+const total = [];
+// Test data: 125, 555 and 44
+
+// Hint: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
