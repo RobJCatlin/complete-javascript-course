ca8bc9315762f59889d4961a2113d3a06bb16824
RobJCatlin <78025477+RobJCatlin@users.noreply.github.com>
2023-03-04 01:03:03 +0000

    function, method practice

--- 19-Coding-Challenges/script.js
+++ 19-Coding-Challenges/script.js
@@ -232,23 +232,38 @@ const mark = {
   fullName: "Mark Miller",
   mass: 78,
   height: 1.69,
-  bmi: (calcBMI = () => {
-    return this.mass / (this.height * this.height);
-  }),
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return;
+  },
 };
 
 const john = {
   fullName: "John Smith",
   mass: 92,
   height: 1.95,
-  bmi: (calcBMI = () => {
-    return this.mass / (this.height * this.height);
-  }),
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return;
+  },
 };
 
 // 2. Create a 'calcBMI' method on each object to calculate the BMI (thesame method on both objects). Store the BMI value to a property, and also return it from the method
+mark.calcBMI();
+john.calcBMI();
 
+console.log(`Mark's height is ${mark.bmi}`);
+console.log(`John's height is ${john.bmi}`);
 // 3. Log to the console who has the higher BMI, together with the full name and the respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
-const message2 = ``;
-console.log(message2);
+// if (mark.bmi > john.bmi) {
+//   console.log(`Mark's BMI (${mark.bmi}) is higher than John's (${john.bmi})!`);
+// } else {
+//   console.log(`John's BMI (${john.bmi}) is higher than Mark's! (${mark.bmi})`);
+// }
+
+mark.bmi > john.bmi
+  ? console.log(`Mark's BMI (${mark.bmi}) is higher than John's (${john.bmi})!`)
+  : console.log(
+      `John's BMI (${john.bmi}) is higher than Mark's! (${mark.bmi})`
+    );
 // Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
