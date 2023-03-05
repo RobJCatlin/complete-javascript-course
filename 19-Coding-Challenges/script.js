cfc8c0efad10178c72d984ea36fb8e88a4c4106d
RobJCatlin <78025477+RobJCatlin@users.noreply.github.com>
2023-03-05 13:23:32 +0000

    loops practice

--- 19-Coding-Challenges/script.js
+++ 19-Coding-Challenges/script.js
@@ -267,3 +267,42 @@ mark.bmi > john.bmi
       `John's BMI (${john.bmi}) is higher than Mark's! (${mark.bmi})`
     );
 // Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Coding Challenge #4
+
+// Let's improve Steven's tip calculator even more, this time using loops!
+// Your tasks:
+
+// 1. Create an array 'bills' containing all 10 test bill values
+const bills2 = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// 2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+const tips2 = [];
+const totals2 = [];
+// 3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate tips and total values (bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations!
+
+const calcTip = (billValue) =>
+  billValue >= 50 && billValue <= 300 ? billValue * 0.15 : billValue * 0.2;
+
+for (let i = 0; i < bills2.length; i++) {
+  tips2.push(calcTip(bills2[i]));
+  totals2.push(bills2[i] + calcTip(bills2[i]));
+}
+
+console.log(tips2);
+console.log(totals2);
+// Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+
+// Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the
+// tips and totals arrays 😉 Bonus:
+
+// 4. Bonus:Write a function 'calcAverage' which takes an array called 'arr' as an argument. This function calculates the average of all numbers in the given array. This is a difficult challenge (we haven't done this before)! Here is how to solve it:
+
+// 4.1. First, you will need to add up all values in the array. To do the addition, start by creating a variable 'sum' that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the 'sum' variable. This way, by the end of the loop, you have all values added together
+
+// 4.2. To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements)
+
+// 4.3. Call the function with the 'totals' array
