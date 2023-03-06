ff04b494b1b70305489e5727a0529361ac7d9ecc
RobJCatlin <78025477+RobJCatlin@users.noreply.github.com>
2023-03-06 23:21:01 +0000

    Developer Skills & Editor Setup - Coding Challenge #1

--- 19-Coding-Challenges/script.js
+++ 19-Coding-Challenges/script.js
@@ -321,3 +321,34 @@ calcAverage2(totals2);
 // 4.2. To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements)
 
 // 4.3. Call the function with the 'totals' array;
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Developer Skills & Editor Setup
+
+// Coding Challenge #1
+
+// Given an array of forecasted maximum temperatures, the thermometer displays a string with the given temperatures. Example: [17, 21, 23] will print "... 17oC in 1 days ... 21oC in 2 days ... 23oC in 3 days ..."
+const forecastedTemps = [17, 21, 23];
+const forecastedTemps2 = [12, 5, -5, 0, 4];
+// Your tasks:
+
+// 1. Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console. Try it with both test datasets.
+const printForecast = arr => {
+  let msg = `It's going to be `;
+  arr.forEach((item, index) => {
+    msg += `${item}oC in ${index + 1} day(s), `;
+  });
+  return msg;
+  // console.log(`17oC in 1 days ... 21oC in 2 days ... 23oC in 3 days ...`);
+};
+// 2. Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+// const print = printForecast(forecastedTemps);
+const print2 = printForecast(forecastedTemps2);
+// console.log(print);
+console.log(print2);
+// Test data:
+// § Data 1: [17, 21, 23]
+// § Data2:[12,5,-5,0,4]
