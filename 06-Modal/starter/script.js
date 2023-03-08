6a7fd5373005928e1ac0c06c7c575ab1c329a265
RobJCatlin <78025477+RobJCatlin@users.noreply.github.com>
2023-03-08 01:11:06 +0000

    event handlers, classList practice

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const modal = document.querySelector('.modal'),
+  overlay = document.querySelector('.overlay'),
+  btnCloseModal = document.querySelector('.close-modal'),
+  btnShowModal = document.querySelectorAll('.show-modal');
+
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  e.key === 'Escape' && !overlay.classList.contains('hidden')
+    ? closeModal()
+    : null;
+});
