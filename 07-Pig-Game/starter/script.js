bb65df690cdb2cba751fae79d42a79bfe63d966e
RobJCatlin <78025477+RobJCatlin@users.noreply.github.com>
2023-03-09 00:57:53 +0000

    Pig Game project

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const score0El = document.querySelector('#score--0'),
+  score1El = document.querySelector('#score--1'),
+  dice = document.querySelector('.dice'),
+  btnNew = document.querySelector('.btn--new'),
+  btnRoll = document.querySelector('.btn--roll'),
+  btnHold = document.querySelector('.btn--hold');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+dice.classList.add('hidden');
+
+let currentScore = 0;
+
+btnRoll.addEventListener('click', function () {
+  const diceRoll = Math.trunc(Math.random() * 6) + 1;
+});
+
+dice.classList.remove('hidden');
+dice.src = `dice-${diceRoll}.png`;
+
+if (diceRoll != 1) {
+  currentScore += diceRoll;
+} else {
+  switchPlayer();
+}
