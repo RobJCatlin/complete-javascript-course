45b48a33b279add232dedc37b647867be133825a
RobJCatlin <78025477+RobJCatlin@users.noreply.github.com>
2023-03-12 00:34:57 +0000

    07-Pig-Game, final file prettier format issue

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -9,11 +9,10 @@ const score0El = document.querySelector('#score--0'),
   player0El = document.querySelector('.player--0'),
   player1El = document.querySelector('.player--1');
 
-let playing = true;
-
 score0El.textContent = 0;
 score1El.textContent = 0;
 dice.classList.add('hidden');
+let playing = true;
 
 const switchPlayers = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -28,41 +27,61 @@ let currentScore = 0;
 let activePlayer = 0;
 
 btnRoll.addEventListener('click', function () {
-  const diceRoll = Math.trunc(Math.random() * 6) + 1;
+  if (playing) {
+    const diceRoll = Math.trunc(Math.random() * 6) + 1;
 
-  dice.classList.remove('hidden');
-  dice.src = `dice-${diceRoll}.png`;
+    dice.classList.remove('hidden');
+    dice.src = `dice-${diceRoll}.png`;
 
-  if (diceRoll != 1) {
-    //add roll value to score
-    currentScore += diceRoll;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore; //change later
-    //switch to next player
-  } else {
-    switchPlayers();
+    if (diceRoll != 1) {
+      //add roll value to score
+      currentScore += diceRoll;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore; //change later
+      //switch to next player
+    } else {
+      switchPlayers();
+    }
   }
 });
 
 btnHold.addEventListener('click', function () {
-  //get current score
-  scores[activePlayer] += currentScore;
-  //show total score
-  document.getElementById(`score--${activePlayer}`).textContent =
-    scores[activePlayer];
+  if (playing) {
+    //get current score
+    scores[activePlayer] += currentScore;
+    //show total score
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
 
-  if (scores[activePlayer] >= 20) {
-    playing = false;
-    dice.classList.add('hidden');
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.add('player--winner');
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.remove('player--active');
-  } else {
-    // Switch to the next player
-    switchPlayers();
+    if (scores[activePlayer] >= 20) {
+      playing = false;
+      dice.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // Switch to the next player
+      switchPlayers();
+    }
   }
-  // switchPlayers();
+});
+
+btnNew.addEventListener('click', function () {
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  currentScore = 0;
+  dice.classList.add('hidden');
+  document.getElementById(`current--${activePlayer}`).textContent =
+    currentScore;
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove('player--winner');
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.add('player--active');
+  playing = true;
+  scores;
 });
