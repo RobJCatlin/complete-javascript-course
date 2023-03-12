bdd3bc75c110f7487d5ce4a8646ebe39388ca8c1
RobJCatlin <78025477+RobJCatlin@users.noreply.github.com>
2023-03-12 22:43:39 +0000

    07-Pig-Game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,18 +1,38 @@
 'use strict';
 
-const score0El = document.querySelector('#score--0'),
-  score1El = document.querySelector('#score--1'),
-  dice = document.querySelector('.dice'),
-  btnNew = document.querySelector('.btn--new'),
-  btnRoll = document.querySelector('.btn--roll'),
-  btnHold = document.querySelector('.btn--hold'),
-  player0El = document.querySelector('.player--0'),
-  player1El = document.querySelector('.player--1');
+// init();
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-dice.classList.add('hidden');
-let playing = true;
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const dice = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  dice.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 
 const switchPlayers = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -21,11 +41,6 @@ const switchPlayers = function () {
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 };
-
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-
 btnRoll.addEventListener('click', function () {
   if (playing) {
     const diceRoll = Math.trunc(Math.random() * 6) + 1;
@@ -33,7 +48,7 @@ btnRoll.addEventListener('click', function () {
     dice.classList.remove('hidden');
     dice.src = `dice-${diceRoll}.png`;
 
-    if (diceRoll != 1) {
+    if (diceRoll !== 1) {
       //add roll value to score
       currentScore += diceRoll;
       document.getElementById(`current--${activePlayer}`).textContent =
@@ -69,19 +84,4 @@ btnHold.addEventListener('click', function () {
   }
 });
 
-btnNew.addEventListener('click', function () {
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  currentScore = 0;
-  dice.classList.add('hidden');
-  document.getElementById(`current--${activePlayer}`).textContent =
-    currentScore;
-  document
-    .querySelector(`.player--${activePlayer}`)
-    .classList.remove('player--winner');
-  document
-    .querySelector(`.player--${activePlayer}`)
-    .classList.add('player--active');
-  playing = true;
-  scores;
-});
+btnNew.addEventListener('click', init);
