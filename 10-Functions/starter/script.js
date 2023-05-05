21b572ec24cba9f7f73d5483ce56ecbf974f1474
RobJCatlin <78025477+RobJCatlin@users.noreply.github.com>
2023-05-05 23:11:07 +0100

    10-Functions .call(), .apply() methods

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -75,6 +75,7 @@ const transformer = (str, fn) => {
   console.log(`Transformed by: ${fn.name}`);
 };
 
+// higher-order function
 transformer('Rob Catlin is dope yo', upperFirstWord);
 console.log('=====================================');
 transformer("how are y'all doing today?", upperFirstLetter);
@@ -84,3 +85,85 @@ const high5 = () => {
 };
 
 document.body.addEventListener('click', high5);
+
+console.log('functions that return functions - promises?');
+
+const greeting = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeting2 = greeting => name => console.log(`${greeting} ${name}`);
+
+const greeterHeya = greeting('Heya');
+
+greeterHeya('Robbie');
+greeterHeya('Jamie');
+
+greeting('Heya')('Rob');
+greeting2('Yo')('Bobby');
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(44, 'Rob Catlin');
+lufthansa.book(635, 'Mike Hunt');
+
+console.log(lufthansa);
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} ${this.iataCode}${flightNum}`
+    );
+  },
+};
+
+//storing the lufthansa book() method in the book variable to be reused
+const book = lufthansa.book;
+console.log(book);
+
+// doesn't work as the 'this' keyword doesn't know what to point to
+// book(877, 'Harry Barry');
+
+eurowings.book(877, 'Harry Barry');
+
+//call method
+console.log(`.call() method`);
+// first argument passed to the call method is what the this keyword will refer to
+book.call(eurowings, 444, 'Parold Barold');
+console.log(eurowings);
+
+book.call(lufthansa, 777, 'Mary Berry');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 583, 'Mary Cooper');
+console.log(swiss);
+
+//apply method
+console.log('.apply() method');
+
+const flightData = [583, 'Bob Cooper'];
+//old way
+book.apply(swiss, flightData);
+//new way using the spread operator
+book.call(swiss, ...flightData);
