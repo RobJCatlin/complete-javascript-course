c60e8402a2424d553c1d22030380a04d14283a1a
RobJCatlin <78025477+RobJCatlin@users.noreply.github.com>
2023-05-09 23:29:20 +0100

    10-Functions - Coding Challenge #1 - Let's build a simple poll app

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -233,18 +233,94 @@ const addVAT2 = addTaxRate(0.23);
 addVAT2(100);
 addVAT2(23);
 
-// const greeting = function (greeting) {
-//   return function (name) {
-//     console.log(`${greeting} ${name}`);
-//   };
-// };
+// A Closer Look at Functions
 
-// const greeting2 = greeting => name => console.log(`${greeting} ${name}`);
+// Coding Challenge #1
+// Let's build a simple poll app!
 
-// const greeterHeya = greeting('Heya');
+// A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter 'poll' object below.
 
-// greeterHeya('Robbie');
-// greeterHeya('Jamie');
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  answers: new Array(4).fill(0),
+  // [0,0,0,0]
+  registerNewAnswer() {
+    const answer = +prompt(
+      `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+    );
 
-// greeting('Heya')('Rob');
-// greeting2('Yo')('Bobby');
+    // answer !== NaN && answer <= 3
+    //   ? this.answers[answer]++
+    //   : this.registerNewAnswer();
+
+    // console.log(this.answers);
+    // console.log(typeof newAnswer);
+    console.log(`answers.length = ${this.answers.length}`);
+    console.log(`answers.length test = ${this.answers[4 - 1]}`);
+
+    //short circuiting - the && makes the last part
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+
+    // console.log(this.answers);
+
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  },
+  //how to give 'type' argument to displayResults()?
+  // displayResults(type) {
+  //   // if (type === typeof string) {
+  //   //   return console.log('string');
+  //   // } else if (type === typeof array) {
+  //   //   return console.log('array');
+  //   // }
+  //   console.log(this.answers);
+  // },
+};
+
+// Your tasks:
+// 1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+// 1.1. Display a prompt window for the user to input the number of the
+// selected option. The prompt should look like this: What is your favourite programming language?
+// 0: JavaScript
+// 1: Python
+// 2: Rust
+// 3: C++
+// (Write option number)
+
+// 1.2. Based on the input number, update the 'answers' array property. For example, if the option is 3, increase the value at position 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g. answer 52 wouldn't make sense, right?)
+
+// 2. Call this method whenever the user clicks the "Answerpoll" button.
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+// 3. Create a method 'displayResults' which displays the poll results.
+// The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1".
+
+// 4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+// 5. Bonus: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do not put the arrays in the poll object! So what should the this keyword look like in this situation?
+
+const data1 = [5, 2, 3];
+const data2 = [1, 5, 3, 9, 6, 1];
+
+poll.displayResults.call({ answers: data1 }, 'string');
+poll.displayResults.call({ answers: data1 });
+poll.displayResults.call({ answers: data2 }, 'string');
+poll.displayResults.call({ answers: data2 });
+
+// Test data for bonus:
+// § Data1:[5,2,3]
+// § Data2:[1,5,3,9,6,1]
+// Hints: Use many of the tools you learned about in this and the last section
