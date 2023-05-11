a47a682b33d39d6749d02f54ce2f72d8f69a3719
RobJCatlin <78025477+RobJCatlin@users.noreply.github.com>
2023-05-11 10:13:30 +0100

    11-Arrays - array methods

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -74,3 +74,54 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+//SLICE
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+//get the last item in an array
+console.log(arr.slice(-1));
+console.log(arr.slice(2, -2));
+//makes a shallow copy of the array
+console.log(arr.slice());
+console.log([...arr]);
+
+//SPLICE - same as SLICE but mutates the original array
+// console.log(arr.splice(2));
+console.log(arr.splice(-1));
+console.log(arr);
+console.log(arr.splice(1, 2));
+console.log(arr);
+
+//REVERSE
+//mutates original array
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+//CONCAT
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+//JOIN
+console.log(letters.join(' - '));
+
+const arr3 = [44, 12, 76];
+//old way
+console.log(arr3[0]);
+//new way
+console.log(arr3.at(0));
+
+//get the last element (old ways)
+console.log(arr3.slice(-1)[0]);
+console.log(arr3[arr3.length - 1]);
+//get the last element (new way)
+console.log(arr3.at(-1));
+
+//also works on strings
+console.log('Rob Catlin'.at(0));
+console.log('Rob Catlin'.at(-1));
