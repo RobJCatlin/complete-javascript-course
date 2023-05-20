697acd98a2085b1d63d34e721bd0d3b6eb4ae8a3
RobJCatlin <78025477+RobJCatlin@users.noreply.github.com>
2023-05-20 01:35:14 +0100

    11-Arrays - method chaining

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -4,6 +4,8 @@
 /////////////////////////////////////////////////
 // BANKIST APP
 
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
 // Data
 const account1 = {
   owner: 'Jonas Schmedtmann',
@@ -81,6 +83,16 @@ displayMovements(account1.movements);
 
 // const user = 'Steven Thomas Williams'; //stw
 
+const calcDisplayBalance = movements => {
+  const mainBalanceEl = document.querySelector('.balance__value');
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  mainBalanceEl.textContent = `${balance} EUR`;
+};
+
+calcDisplayBalance(account1.movements);
+
+// const calcDisplaySummary =
+
 const createUserNames = accs => {
   accs.forEach(acc => {
     acc.username = acc.owner
@@ -94,16 +106,24 @@ const createUserNames = accs => {
 };
 
 createUserNames(accounts);
-console.log(accounts);
+// console.log(accounts);
 
-const calcPrintedBalance = movements => {};
 // console.log(userName);
+
+const max = movements.reduce(
+  (acc, mov) => (acc > mov ? acc : mov),
+  movements[0]
+);
+// return acc > mov ? (acc = mov) : mov; // incorrect logic, think about things!!
+
+// console.log(movements);
+// console.log(max);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -270,13 +290,66 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // console.log(depositsForOf);
 
 //reduce method
-console.log(movements);
+// console.log(movements);
 //accumulator is like a snowball
 // const balance = movements.reduce((acc, current, i, array) => {
 //   console.log(`Iteration ${i + 1}: Current value: ${acc}`);
 //   return acc + current;
 // }, 0);
 
-const balance = movements.reduce((acc, current, i, array) => acc + current, 0);
+// const balance = movements.reduce((acc, current, i, array) => acc + current, 0);
 
-console.log(balance);
+// console.log(balance);
+
+// Coding Challenge #2
+// Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+// Your tasks:
+// Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+const calcAverageHumanAge = ages => {
+  const inHumanYears = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+  // console.log(inHumanYears);
+
+  const adultDogs = inHumanYears.filter(age => age > 17);
+  // console.log(adultDogs);
+
+  const humanAge =
+    adultDogs.reduce((acc, val) => acc + val, 0) / adultDogs.length;
+  return humanAge;
+  // console.log(`Human Age: ${humanAge}`);
+  // const averageHumanAge = humanAge / adultDogs.length;
+  // console.log(humanAge);
+};
+
+const avr1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avr2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+
+console.log(avr1);
+console.log(avr2);
+
+// 1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4
+
+// 2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+
+// 3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+
+// 4. Run the function for both test data sets
+
+// Test data:
+// Data1:[5,2,4,1,15,8,3]
+// Data2:[16,6,10,5,6,1,4]
+
+const euroToUsd = 1.1;
+
+//pipeline
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map(mov => mov * euroToUsd)
+  // .map((mov, i, arr) => {
+  //   console.log(arr);
+  //   return mov * euroToUsd;
+  // })
+  .reduce((acc, mov) => acc + mov, 0);
+
+console.log(totalDepositsUSD);
