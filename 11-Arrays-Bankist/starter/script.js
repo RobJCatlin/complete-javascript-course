4c2b65ac182cecbb4a925faace673c6926357001
RobJCatlin <78025477+RobJCatlin@users.noreply.github.com>
2023-06-24 23:40:18 +0100

    11-Arrays - Coding Challenge #4

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -670,19 +670,29 @@ const dogs = [
 
 // 1. Loop over the 'dogs' array containing dog objects, and for each dog, calculate the recommended food portion and add it to the object as a new property. Do not create a new array, simply loop over the array. Forumla: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the weight needs to be in kg)
 
-const recommendedFoodWeight = weight => Math.round(weight ** 0.75 * 28);
-const foodCalc = (current, recommended) => {
-  return current > recommended * 0.9 && current < recommended * 1.1;
-};
-
 dogs.forEach(dog => {
-  dog.foodweight = recommendedFoodWeight(dog.weight);
-  dog.owners.includes('Sarah') && foodCalc(dog.curFood, dog.foodweight);
+  dog.recFood = Math.trunc(dog.weight ** 0.75 * 28);
 });
+console.log(dogs);
 
 // 2. Find Sarah's dog and log to the console whether it's eating too much or too little. Hint: Some dogs have multiple owners, so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose)
+const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+console.log(dogSarah);
+console.log(
+  `Sarah's dog eats too ${
+    dogSarah.recFood < dogSarah.curFood ? 'much' : 'little'
+  }`
+);
 
 // 3. Create an array containing all owners of dogs who eat too much ('ownersEatTooMuch') and an array with all owners of dogs who eat too little ('ownersEatTooLittle').
+const ownersEatTooMuch = dogs.filter(dog => dog.recFood < dog.curFood);
+ownersEatTooMuch.forEach(dog => {
+  console.log(dog.owners);
+});
+// console.log(ownersEatTooMuch);
+
+// const ownersEatTooLittle = dogs.filter(dog => dog.recFood > dog.curFood);
+// console.log(ownersEatTooLittle);
 
 // 4. Log a string to the console for each array created in 3,like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
 
