1354d215810812479546e153bbc03bfb257d83cd
RobJCatlin <78025477+RobJCatlin@users.noreply.github.com>
2023-06-26 23:34:30 +0100

    12-Numbers, Dates, Timers

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -251,3 +251,26 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+// parsing
+console.log(Number.parseInt('30px', 10));
+console.log(Number.parseInt('m25', 10));
+
+console.log(Number.parseInt('    2.5    ', 10));
+console.log(Number.parseInt('2.5', 10));
+console.log(Number.parseFloat('2.5', 10));
+
+console.log(Number.isNaN(20));
+console.log(Number.isNaN('20'));
+console.log(Number.isNaN(+'20x'));
+console.log(Number.isNaN(23 / 0));
+
+// the isFinite() method is the best way of checking if something is a number
+console.log(Number.isFinite(20));
+console.log(Number.isFinite('20'));
+console.log(Number.isFinite(+'20x'));
+console.log(Number.isFinite(23 / 0));
+
+console.log(Number.isIntager(20));
+console.log(Number.isIntager(23.0));
+console.log(Number.isIntager(23 / 0));
