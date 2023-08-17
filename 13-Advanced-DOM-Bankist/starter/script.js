486faa251021c3e8d89280668209be74a48daad1
RobJCatlin <78025477+RobJCatlin@users.noreply.github.com>
2023-08-17 16:40:08 +0100

    13-Advanced-DOM - add, remove, edit DOM nodes, attributes

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -1,125 +1,155 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
+// ///////////////////////////////////////
+// // Modal window
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+// const modal = document.querySelector('.modal');
+// const overlay = document.querySelector('.overlay');
+// const btnCloseModal = document.querySelector('.btn--close-modal');
+// const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function (e) {
-  e.preventDefault();
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
+// const openModal = function (e) {
+//   e.preventDefault();
+//   modal.classList.remove('hidden');
+//   overlay.classList.remove('hidden');
+// };
 
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
+// const closeModal = function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// };
 
-// for (let i = 0; i < btnsOpenModal.length; i++)
-//   btnsOpenModal[i].addEventListener('click', openModal);
+// // for (let i = 0; i < btnsOpenModal.length; i++)
+// //   btnsOpenModal[i].addEventListener('click', openModal);
 
-btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+// btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+// btnCloseModal.addEventListener('click', closeModal);
+// overlay.addEventListener('click', closeModal);
 
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
-  }
+// document.addEventListener('keydown', function (e) {
+//   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+//     closeModal();
+//   }
+// });
+
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
+
+// const header = document.querySelector('.header');
+// const allSections = document.querySelectorAll('.section');
+// console.log(allSections);
+
+// document.getElementById('section--1');
+// const allButtons = document.getElementsByTagName('button');
+// console.log(allButtons);
+
+// console.log(document.getElementsByClassName('btn'));
+
+// // Creating and inserting elements
+// // .insertAdjacentHTML
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// message.textContent =
+//   'We use cookies for improved functionailty and analytics.';
+// message.innerHTML =
+//   'We use cookies for improved functionailty and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// // header.prepend(message);
+// header.append(message);
+// // header.append(message.cloneNode(true));
+
+// // header.before(message);
+// // header.after(message);
+
+// // Delete elements
+// document.querySelector('.btn--close-cookie').addEventListener(
+//   'click',
+//   () => message.remove()
+//   //message.parentElemet.removeChild(message) //old way before .remove()
+// );
+
+// // Styles
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '120%';
+
+// // doesn't work on normal styles
+// console.log(message.style.height);
+// // works on inline styles
+// console.log(message.style.backgroundColor);
+
+// console.log(getComputedStyle(message).color);
+// console.log(getComputedStyle(message).height);
+
+// message.style.height =
+//   Number.parseInt(getComputedStyle(message).height, 10) + 30 + 'px';
+
+// // change custom properties using setProperty()
+// // this changes the css variables in :root{}
+// // could use this for dark mode?
+// document.documentElement.style.setProperty('--color-primary', 'pink');
+
+// // Attributes
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
+// console.log(logo.src);
+
+// logo.alt = 'Beautiful minimalist logo';
+
+// // Non-standard attribute
+// console.log(logo.designer);
+// console.log(logo.getAttribute('designer'));
+// logo.setAttribute('company', 'bankist');
+
+// console.log(logo.src);
+// console.log(logo.getAttribute('src'));
+
+// const link = document.querySelector('.nav__link--btn');
+
+// console.log(link.href);
+// console.log(link.getAttribute('href'));
+
+// // data attributes
+// // can get the values of data-**** attributes, but needs to be camelCase
+// // data-version-number
+// console.log(logo.dataset.versionNumber);
+
+// // Classes
+// logo.classList.add('c');
+// logo.classList.remove('c');
+// logo.classList.toggle('c');
+// logo.classList.contains('c');
+
+// // Don't use this as it will replace all class attributes with this value
+// console.log((logo.className = 'example'));
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', e => {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  console.log(e.target.getBoundingClientRect());
+  console.log('Current scroll (X/Y)', window.scrollX, window.scrollY);
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.scrollX,
+  //   s1coords.top + window.scrollY
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.scrollX,
+  //   top: s1coords.top + window.scrollY,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({
+    behavior: 'smooth',
+  });
 });
-
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
-
-const header = document.querySelector('.header');
-const allSections = document.querySelectorAll('.section');
-console.log(allSections);
-
-document.getElementById('section--1');
-const allButtons = document.getElementsByTagName('button');
-console.log(allButtons);
-
-console.log(document.getElementsByClassName('btn'));
-
-// Creating and inserting elements
-// .insertAdjacentHTML
-const message = document.createElement('div');
-message.classList.add('cookie-message');
-message.textContent =
-  'We use cookies for improved functionailty and analytics.';
-message.innerHTML =
-  'We use cookies for improved functionailty and analytics. <button class="btn btn--close-cookie">Got it!</button>';
-
-// header.prepend(message);
-header.append(message);
-// header.append(message.cloneNode(true));
-
-// header.before(message);
-// header.after(message);
-
-// Delete elements
-document.querySelector('.btn--close-cookie').addEventListener(
-  'click',
-  () => message.remove()
-  //message.parentElemet.removeChild(message) //old way before .remove()
-);
-
-// Styles
-message.style.backgroundColor = '#37383d';
-message.style.width = '120%';
-
-// doesn't work on normal styles
-console.log(message.style.height);
-// works on inline styles
-console.log(message.style.backgroundColor);
-
-console.log(getComputedStyle(message).color);
-console.log(getComputedStyle(message).height);
-
-message.style.height =
-  Number.parseInt(getComputedStyle(message).height, 10) + 30 + 'px';
-
-// change custom properties using setProperty()
-// this changes the css variables in :root{}
-// could use this for dark mode?
-document.documentElement.style.setProperty('--color-primary', 'pink');
-
-// Attributes
-const logo = document.querySelector('.nav__logo');
-console.log(logo.alt);
-console.log(logo.src);
-
-logo.alt = 'Beautiful minimalist logo';
-
-// Non-standard attribute
-console.log(logo.designer);
-console.log(logo.getAttribute('designer'));
-logo.setAttribute('company', 'bankist');
-
-console.log(logo.src);
-console.log(logo.getAttribute('src'));
-
-const link = document.querySelector('.nav__link--btn');
-
-console.log(link.href);
-console.log(link.getAttribute('href'));
-
-// data attributes
-// can get the values of data-**** attributes, but needs to be camelCase
-// data-version-number
-console.log(logo.dataset.versionNumber);
-
-// Classes
-logo.classList.add('c');
-logo.classList.remove('c');
-logo.classList.toggle('c');
-logo.classList.contains('c');
-
-// Don't use this as it will replace all class attributes with this value
-console.log((logo.className = 'example'));
