c9fee63a730474b5eef4153f1c05b5fc3ebd43bf
RobJCatlin <78025477+RobJCatlin@users.noreply.github.com>
2023-12-06 13:53:48 +0000

    08-Behind-The-Scenes: this keyword

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+// console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  // console.log(this);
+};
+
+calcAge(1984);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  // console.log(this);
+};
+
+calcAgeArrow(1984);
+
+const rob = {
+  year: 1900,
+  calcAge: function (birthYear) {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+
+rob.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = rob.calcAge;
+matilda.calcAge();
+
+const f = rob.calcAge;
